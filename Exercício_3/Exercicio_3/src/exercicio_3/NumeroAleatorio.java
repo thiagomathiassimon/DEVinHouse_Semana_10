d2486c86c82cf83aef8d3ac37d11cf7e7735832b
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-02-10 15:49:36 -0300

    Exercício_3

---
+++ Exercício_3/Exercicio_3/src/exercicio_3/NumeroAleatorio.java
@@ -0,0 +1,15 @@
+package exercicio_3;
+
+import java.util.Random;
+
+public class NumeroAleatorio {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		System.out.println(random.nextDouble());
+
+	}
+
+}
